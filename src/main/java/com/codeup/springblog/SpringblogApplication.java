a3646af2f7708840c4d9b73f50e65f497d1c5842
wilfredopacheco <wilfredo.pacheco.sa@gmail.com>
2018-10-29 09:46:57 -0500

    commit

---
+++ src/main/java/com/codeup/springblog/SpringblogApplication.java
@@ -0,0 +1,12 @@
+package com.codeup.springblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringblogApplication.class, args);
+    }
+}
